954be55b5ef85e10a566e74f20679ce99914971c
Michael Schmitz <Michael.Schmitz@T-Systems.com>
2016-01-08 17:38:40 +0100

    Erstellung Plot3

---
+++ plot3.r
@@ -0,0 +1,16 @@
+# xTicks are german locale for "Thu, Fri, Sat", so it would be
+# rather time-consuming trying to change them
+with(powerConsum, {
+	plot(datetime, Sub_metering_1, type = "n",
+		xlab = "", ylab = "Energy sub metering")
+	lines(datetime, Sub_metering_1, col = "black")
+	lines(datetime, Sub_metering_2, col = "red")
+	lines(datetime, Sub_metering_3, col = "blue")
+	legend("topright", col = c("black", "red", "blue"), lwd = 1,
+		legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+	}
+)
+# Write plot as PNG file with given size
+dev.copy(png, file="plot3.png", width = 480, height = 480)
+# Close device
+dev.off()
